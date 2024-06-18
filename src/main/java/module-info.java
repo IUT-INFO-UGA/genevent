e6b9e2ac22ac11ff8206a4dda96ac4c2350ae8f8
Salamint <ndupont2810@gmail.com>
2024-06-18 13:34:56 +0200

    Vue d'ajout d'un jeu de société au stock terminé (ajout et suppression).

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -7,6 +7,7 @@ module genevent {
 
     opens fr.uga.iut2.genevent.vue to javafx.fxml;
     opens fr.uga.iut2.genevent.modele.membre to javafx.base;
+    opens fr.uga.iut2.genevent.modele.jeu to javafx.base;
 
     exports fr.uga.iut2.genevent;
 }
